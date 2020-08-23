f63df22aa9f8aedc4f2d4fb69dc66755809451c8
David Buffkin <david@Davids-MacBook-Pro.local>
2020-08-23 19:44:07 -0400

    Increased usage of makedata.py

--- makedata.py
+++ makedata.py
@@ -1,43 +1,71 @@
 #David Buffkin
 
-#Takes a crossword in JSON and outputs a txt file containing clue answer pairs.
-#Usage: python makedata.py <inputfile> [outputfile]
+#Takes a directory or file containing crossword(s) in JSON and outputs a txt file containing clue answer pairs.
+#Usage: python makedata.py <input> <outputfile>
 
 import json
 import sys
-import os.path
+from os.path import splitext
+from os import walk
 
-filename, extension = os.path.splitext(sys.argv[1])
 
-if extension != ".json":
-    print("Not a JSON file, exiting.")
-    sys.exit
+if len(sys.argv) < 2:
+    print("No file or directory supplied, exiting.")
+    sys.exit()
+if len(sys.argv) < 3:
+    print("No output file specified, exiting.")
+    sys.exit()
 
-file_raw = None
-with open(filename + extension) as reader:
-    file_raw = reader.read()
+#Get all files in directory
+f = []
+for (dirpath, dirnames, filenames) in walk(sys.argv[1]):
+    f += [dirpath + "/" + name for name in  filenames]
 
-cross = json.loads(file_raw)
+if len(f) == 0:
+    f = [sys.argv[1]]
 
-writeName = filename + "_pairs.txt" if len(sys.argv) < 3 else sys.argv[2]
+
 
 def getClue(clue_raw):
     return clue_raw[clue_raw.find(" ") + 1:]
 
-with open(writeName, "w") as writer:
+def writeCross(cross, writer):
     acrossClues = cross['clues']['across']
     acrossAnswers = cross['answers']['across']
     if len(acrossAnswers) != len(acrossClues):
-        print("Puzzle is corrupted, exiting.")
+        print("Puzzle is corrupted, exiting")
         sys.exit()
     for i in range(len(acrossAnswers)):
         if i != 0:
             writer.write("\n")
-        writer.writelines(f"{getClue(acrossClues[i])}\n{acrossAnswers[i]}")
+        clue = getClue(acrossClues[i]).replace("|", " ")
+        answer = acrossAnswers[i].replace("|", " ")
+        writer.write(f"{clue}|{answer}")
     downClues = cross['clues']['down']
     downAnswers = cross['answers']['down']
     if len(downAnswers) != len(downClues):
         print("Puzzle is corrupted, exiting.")
         sys.exit()
     for i in range(len(downAnswers)):
-        writer.write(f"\n{getClue(downClues[i])}\n{downAnswers[i]}")
+        clue = getClue(downClues[i]).replace("|", " ")
+        answer = downAnswers[i].replace("|", " ")
+        writer.write(f"\n{clue}|{answer}")
+
+first = True
+with open(sys.argv[2], "w") as writer:
+    for filename in f:
+        if not first:
+            writer.write("\n")
+        first = False
+        if splitext(filename)[1] != ".json":
+            continue
+        try:
+            file_raw = None
+            with open(filename) as reader:
+                file_raw = reader.read()
+
+            cross = json.loads(file_raw)
+            
+            writeCross(cross, writer)
+        except:
+            continue
